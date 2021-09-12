440e029e8853ecb11acc35b6ccfb5bff383dc405
danielkwok21 <danielzhengxian21@gmail.com>
2021-09-12 16:16:21 +0800

    first commit

---
+++ be-node.js
@@ -0,0 +1,45 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const { query } = require('./db')
+app.use(bodyParser.urlencoded({ extended: 'true' }));
+app.use(bodyParser.json());
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, AUTHORIZATION');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.get('/reviews', async (req, res) => {
+    const q0 = 'SELECT * FROM REVIEWS'
+    const reviews = await query(q0)
+
+    res.json({
+        status: 200,
+        reviews
+    })
+})
+
+app.get('/products', async (req, res) => {
+    const q0 = 'SELECT * FROM PRODUCTS LIMIT 1'
+    const product = await query(q0)
+
+    res.json({
+        status: 200,
+        product
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World from notion-challenge-be-node')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ db/index.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+const { config } = require('../private')
+
+const db = mysql.createConnection({
+    host: config.db.host,
+    user: config.db.user,
+    password: config.db.password
+});
+
+db.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+function query(queryString) {
+    return new Promise((res, rej) => {
+        return db.query(queryString, (err, result) => {
+            if (err) {
+                rej(err)
+            } else {
+                res(result)
+            }
+        })
+    })
+}
+
+module.exports = {
+    query
+}
